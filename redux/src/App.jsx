20f953ef44b9ca6c67e27ceef917b2b631caeb14
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-08 09:49:02 +0530

    deployment completed

---
+++ redux/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LikeCounter from './components/LikeCounter'
+// import LikeCounter from './component/LikeCounter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LikeCounter/>
+    </>
+  )
+}
+
+export default App
